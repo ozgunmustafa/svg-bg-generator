5f1b84d68c33028b1df995cdb94d0f44a7bfb31f
Flarmoni <mustiozgun@gmail.com>
2023-02-11 23:29:59 +0300

    react-dnd added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
+import React, { useState } from 'react';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
+import LayerCard from './components/LayerCard';
+import { listData } from './data/listData';
+
+const LayerList = React.memo(function LayerList({ layers }) {
+  return layers.map((item, index) => (
+    <LayerCard item={item} index={index} key={index} />
+  ));
+});
+
 function App() {
+  const defaultList = ['A', 'B', 'C', 'D', 'E'];
+
+  const [itemList, setItemList] = useState(listData);
+  const handleDrop = (droppedItem) => {
+    if (!droppedItem.destination) return;
+    var updatedList = [...itemList];
+    const [reorderedItem] = updatedList.splice(droppedItem.source.index, 1);
+    updatedList.splice(droppedItem.destination.index, 0, reorderedItem);
+    setItemList(updatedList);
+  };
   return (
     <div className="App">
       <div className="general-grid">
-        <div className="general-grid_left"></div>
+        <div className="general-grid_left">
+          <DragDropContext onDragEnd={handleDrop}>
+            <Droppable droppableId="list-container">
+              {(provided) => (
+                <div {...provided.droppableProps} ref={provided.innerRef}>
+                  <LayerList layers={itemList} />
+                  {provided.placeholder}
+                </div>
+              )}
+            </Droppable>
+          </DragDropContext>
+        </div>
         <div className="general-grid_center"></div>
         <div className="general-grid_right"></div>
       </div>
---
+++ src/data/listData.js
@@ -0,0 +1,36 @@
+const shapeIcon = require('../assets/img/shape.svg').default;
+const imageIcon = require('../assets/img/image.svg').default;
+const textIcon = require('../assets/img/text.svg').default;
+
+export const listData = [
+  {
+    id: '1',
+    icon: shapeIcon,
+    type: 'shape',
+    title: 'Learn Angular in 21 days',
+  },
+  {
+    id: '2',
+    icon: shapeIcon,
+    type: 'shape',
+    title: 'Learn Angular in 21 days',
+  },
+  {
+    id: '3',
+    icon: textIcon,
+    type: 'text',
+    title: 'Learn Angular in 21 days',
+  },
+  {
+    id: '4',
+    icon: shapeIcon,
+    type: 'shape',
+    title: 'Learn Angular in 21 days',
+  },
+  {
+    id: '5',
+    icon: imageIcon,
+    type: 'image',
+    title: 'Learn Angular in 21 days',
+  },
+];
